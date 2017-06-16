73709a654357474801f8c27b3942c6ac14992b49
Timofte Alexandru <timofte.alexandru05@yahoo.com>
2017-06-16 16:04:13 +0300

    added socket.io for communication

--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ import cors from 'cors';
 import webpack from 'webpack';
 import webpackConfig from './../webpack.config'
 import webpackDevMiddleware from 'webpack-dev-middleware';
-const compiler = webpack(webpackConfig);
 import webpackHotMiddleware from "webpack-hot-middleware";
+import socketIO from 'socket.io';
+
+const compiler = webpack(webpackConfig);
+
 
 import {
     channels,
@@ -17,6 +20,7 @@ import {
 
 let app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(cors());
 app.use(webpackDevMiddleware(compiler, {
@@ -114,3 +118,6 @@ const port = 9000;
 server.listen(port,()=>{
     console.info(`Redux Messenger is listening on port ${port}.`);
 });
+
+import { simulateActivity } from './simulateActivity';
+simulateActivity(currentUser.id);
--- src/getStore.js
+++ src/getStore.js
@@ -1,15 +1,22 @@
 import {
     createStore,
-    applyMiddleware
+    applyMiddleware,
+    compose
 } from 'redux';
 
 import {
     fromJS
 } from 'immutable'
 
+import { createSocketMiddleware } from './socketMiddleware';
 import {
     users,
 } from './../server/db';
+
+import {
+    RECEIVE_MESSAGE
+} from './actions';
+
 
 import {
     getDefaultState,
@@ -23,12 +30,38 @@ initializeDB();
 import { createLogger } from 'redux-logger'
 import { reducer } from './reducers';
 
+const socketConfigOut = {
+    UPDATE_STATUS: (data)=>({
+        type: `UPDATE_USER_STATUS`,
+        status:data
+    })
+};
+
+const socketMiddleware = createSocketMiddleware(io)(socketConfigOut);
+const logger = createLogger({
+  stateTransformer:state=>state.toJS()
+});
+const enhancer = compose(
+    applyMiddleware(
+        socketMiddleware,
+        logger
+    )
+);
 const currentUser = users[0];
 const defaultState = fromJS(getDefaultState(currentUser));
-const store = createStore(reducer,defaultState,applyMiddleware(createLogger({
-    stateTransformer:state=>state.toJS()
-})));
-
+const store = createStore(reducer,defaultState,enhancer);
+const socketConfigIn = {
+    NEW_MESSAGE:(data)=>({
+        type:RECEIVE_MESSAGE,
+      message:data
+    })
+};
+const socket = io();
+for (const key in socketConfigIn){
+    socket.on(key,data=>{
+        store.dispatch(socketConfigIn[key](data));
+    })
+}
 // console.log(defaultState);
-console.log(store.getState().toJS());
+// console.log(store.getState().toJS());
 export const getStore = ()=>store;
